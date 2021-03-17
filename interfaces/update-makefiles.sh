b4bdb30cf57311a766dc43b10d742d684c355282
Xayah <1249475336@qq.com>
2021-03-17 11:44:34 +0800

    fix interfaces path

--- interfaces/update-makefiles.sh
+++ interfaces/update-makefiles.sh
@@ -3,4 +3,4 @@
 source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
-  "vendor.xiaomi:device/xiaomi/cas/interfaces"
+  "vendor.xiaomi:device/xiaomi/cezanne/interfaces"
