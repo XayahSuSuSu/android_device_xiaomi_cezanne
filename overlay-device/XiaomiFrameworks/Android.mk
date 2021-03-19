c2fe0956e9dbfd256338bd30279fd326221f2be0
Xayah <1249475336@qq.com>
2021-03-19 12:30:02 +0800

    modify overlays

---
+++ overlay-device/XiaomiFrameworks/Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_PACKAGE_NAME := XiaomiFrameworks
+LOCAL_MODULE_OWNER := xiaomi
+LOCAL_MODULE_TAGS := optional
+LOCAL_PRODUCT_MODULE := true
+LOCAL_CERTIFICATE := platform
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_SDK_VERSION := current
+include $(BUILD_RRO_PACKAGE)
