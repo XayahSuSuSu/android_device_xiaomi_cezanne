f42f95e745fa3e7e7bcc7a05e8e447164f04f7ea
Xayah <1249475336@qq.com>
2021-03-19 21:39:59 +0800

    set selinux permissive

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -85,6 +85,7 @@ BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
 BOARD_MKBOOTIMG_ARGS += --pagesize $(BOARD_KERNEL_PAGESIZE) --board ""
 
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 
 # Partitions
