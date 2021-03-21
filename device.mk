621512f8e91fecf9918a4865a8324b4e08617efc
Xayah <1249475336@qq.com>
2021-03-21 17:38:22 +0800

    Revert "Overlays: Update"
    This reverts commit 1c83ab4991257868e6fd54f39c10f7ac71fe8a4d.

--- device.mk
+++ device.mk
@@ -25,7 +25,8 @@ PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-system
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-arrow
 
 # Overlays -- Override vendor ones
 PRODUCT_PACKAGES += \
