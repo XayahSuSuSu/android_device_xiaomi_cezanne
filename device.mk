8251bc25397d0db9662d43a3d90d1cd16f96ef27
Xayah <1249475336@qq.com>
2021-09-14 00:15:21 +0800

    Revert "cezanne: Build IMS From MTK OSS"
    This reverts commit 11bddb1111af2045d0af88f2e5f1be9800202c4a.

--- device.mk
+++ device.mk
@@ -16,9 +16,6 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/xiaomi/cezanne/cezanne-vendor.mk)
 
-# IMS
-$(call inherit-product, vendor/mediatek/ims/mtk-ims.mk)
-
 # Inherit properties
 include $(LOCAL_PATH)/properties.mk
 PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
@@ -103,10 +100,6 @@ PRODUCT_PACKAGES += \
 # HotwordEnrollement
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
-
-# ImsInit hack
-PRODUCT_PACKAGES += \
-    ImsInit
 
 # IFAA manager
 PRODUCT_PACKAGES += \
